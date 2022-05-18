72aec0b86aeec9a5b416a32ed7f883b33bebc391
shivanibhadani <shivanibhadani30@gmail.com>
2022-05-18 17:09:33 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+
+namespace task_2_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\VIRAJ\Documents\ridhhi.accdb;Persist Security Info=True");
+            OleDbCommand cmd = con.CreateCommand();
+            con.Open();
+            cmd.CommandText = "Insert into table1(nm,cm,em,msg) values('" + txtusername.Text + "','" + txtcompany.Text + "','" + txtemail.Text + "','" + txtmsg.Text + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            MessageBox.Show(" submited successfully ", "message");
+            // this.Close();
+            con.Close();
+            contact_us new_con = new contact_us();
+            new_con.Show();
+        }
+    }
+}
---
+++ contact_us.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace task_2_
+{
+    /// <summary>
+    /// Interaction logic for contact_us.xaml
+    /// </summary>
+    public partial class contact_us : Window
+    {
+        public contact_us()
+        {
+            InitializeComponent();
+            gridload();
+        }
+        private void gridload()
+        {
+            OleDbConnection con = new OleDbConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ToString();
+            con.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "select * from [table1]";
+            cmd.Connection = con;
+            OleDbDataReader db = cmd.ExecuteReader();
+            grid1.ItemsSource = db;
+
+        }
+    }
+}
